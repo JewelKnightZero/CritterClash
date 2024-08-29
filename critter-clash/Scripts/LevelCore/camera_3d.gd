39dc9f91249a99c2287eee0f2ff57e950f0febb9
Jessie Kyland Frederick Mancer <Kyland.F.Mancer@student.uts.edu.au>
2024-08-30 01:33:37 +1000

    Camera Movement

---
+++ critter-clash/Scripts/LevelCore/camera_3d.gd
@@ -0,0 +1,25 @@
+extends Camera3D
+@onready var playerManager: Node3D = %PlayerManager
+var maxDistance:float = 0
+var maxXDistance:float = 0
+var maxYDistance:float = 0
+const startingDistance: float = 10
+const maxCamDistance:float = 30
+const minCamDistance:float = 5
+const camSpeed:float = 100
+
+func _physics_process(delta: float) -> void:
+	#Move the camera to show all players.
+	#First we find the max distance to a player
+	maxXDistance = minCamDistance #Reset each frame to minimum distance
+	maxYDistance = minCamDistance
+	for child: CharacterBody3D in playerManager.get_children(): # Loop through players to find the furthest distances
+		if maxXDistance < abs(child.global_position.x):
+			maxXDistance = abs(child.global_position.x)
+		if maxYDistance < abs(child.global_position.y):
+			maxYDistance = abs(child.global_position.y)
+	maxDistance = maxXDistance if maxXDistance > maxYDistance else maxYDistance #Reads surprisingly well.returns the bigger distance
+	if maxDistance > maxCamDistance:
+		maxDistance = maxCamDistance
+	
+	position.z = move_toward(position.z, maxDistance, delta * camSpeed)
